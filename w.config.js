329fb2a96b8c84edc10439eacb50a8e92bb02104
hansen666666 <hansen666666@devide>
2023-08-17 17:11:34 +0800

    Thu Aug 17 17:11:34 CST 2023 inscode

--- w.config.js
+++ w.config.js
@@ -58,9 +58,9 @@ var devPlugins =  [
   // 允许错误不打断程序, 仅开发模式需要
   new webpack.NoErrorsPlugin(),
   // 打开浏览器页面
-  new OpenBrowserPlugin({
-    url: 'http://127.0.0.1:8080/'
-  }),
+  // new OpenBrowserPlugin({
+  //   url: 'http://0.0.0.0:8080/'
+  // }),
   // css打包
   new ExtractTextPlugin('css.css', {
     allChunks: true
